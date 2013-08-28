fc071fd0c345ce95fe0e23fdb32124ff93d7186e
damon <398846606@qq.com>
2013-08-29 00:26:18 +0800

    update

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 <link href="css/layout.css" rel="stylesheet">
 <script type="text/javascript" src="js/selectUI.js"></script>
 </head>
-<body>
+<body id="body">
+    <div class="test">
     <form method="get" action="action.php">
         <div class="row">
             <span class="label">Your name:</span>
@@ -25,20 +26,63 @@
                 <option>Java</option>
             </select>
         </div>
-        <div class="row">
+        <div id="row">
             <input type="submit" name="submitBtn" id="submit">
         </div>
     </form>
+    </div>
 </body>
 </html>
 <script type="text/javascript">
-query('#row');
+alert(document.getElementById('body').nodeType)
+// query('body .test form div.row');
 function query(selector){
-    var classNameReg = /^\.\S[\w\d]*/;
+    var _list = [], 
+        temp, 
+        i = 0, 
+        j = 0, 
+        _el = [];
+
+    // var classNameReg = /^\.\S[\w\d]*/;
     var idNameReg = /^\#\S[\w\d]*/;
+    var classNameReg = /[\w\d]*\.\S[\w\d]*/;
     // var a = selector.match(idNameReg);
-    var idSelector = classNameReg.test(selector);
-    alert(a);
+    // var idSelector = idNameReg.test(selector);
+    // var classSelector = classNameReg.test(selector);
+    // alert(class2.test(selector));
+    if(/^\#\S[\w\d]*/.test(selector)){
+        temp = selector.substr(1, selector.length - 1);
+        _list.push(document.getElementById(temp));
+    } else if(classNameReg.test(selector)){
+        while(true){
+            if(selector.indexOf(' ') != -1){
+                i = selector.indexOf(' ');
+                temp = selector.substr(j, i);
+                selector = selector.substr(i + 1, selector.length - 1);
+                if(!classNameReg.test(temp)){
+                    _el.push(document.getElementsByTagName(temp));
+                }
+            } else if(selector.indexOf(' ') == -1 && selector.indexOf('.')){
+                i = selector.indexOf('.');
+                temp = selector.substr(j, i);
+                selector = selector.substr(i + 1, selector.length - 1);
+                break;
+            }
+            j = i;
+        }
+
+
+        // var _el = document.getElementsByTagName('*');
+        // temp = selector.substr(1, selector.length - 1);
+        // for(var i = 0; i < _el.length; i++){
+        //     if(_el[i].className == temp){
+        //         _list.push(_el[i]);
+        //     }  
+        // }
+    }
+    for(var i = 0; i < _list.length; i++){
+        // alert(_list[i]);
+    }
 }
 
 
